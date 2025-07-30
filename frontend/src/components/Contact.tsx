afe1a130c36a640d746d086b99317b11a6835b57
Samforquer <samforquer@gmail.com>
2025-07-29 22:01:24 -0400

    test

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -2,5 +2,6 @@ export const Contact: React.FC = () => (
   <section id="contact" className="container py-5">
     <h2 className="mb-4">Contact</h2>
     {/* Your contact info here */}
+    <h3 className="mb-4">In progress...</h3>
   </section>
 )
